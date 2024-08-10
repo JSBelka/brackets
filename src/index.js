d39ea5853f1439f5fdd997894a3fad04ed281d84
JSBelka <barbi-bar@mail.ru>
2024-08-10 15:30:08 +0300

    index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
   let result = true;
-  bracketsConfig.flat().join(', ');
+  bracketsConfig.join(', ');
   for (let i = 0; i < str.length; i ++) {
     if(!bracketsConfig.includes(str[i])) {
       result = false;
